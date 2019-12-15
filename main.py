52af167b29f89d8aa402fc639364a34bc7c34131
Miles Budden <miles@budden.net>
2019-12-15 21:46:24 +0000

    It works for 1 location

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import os
 import datetime
 from pprint import pprint
+import spotipy
+import spotipy.util as util
 
 def get_events(lat, long, rad, eventcode="LIVE", start_date=datetime.datetime.now(), stop_date=datetime.datetime.now()+datetime.timedelta(days=10), description=1, limit=100):
     r = requests.get("https://www.skiddle.com/api/v1/events/search/",
@@ -12,8 +14,8 @@ def get_events(lat, long, rad, eventcode="LIVE", start_date=datetime.datetime.no
                         "radius": rad,
                         "eventcode": eventcode,
                         "order": "trending",
-                        "description": 1,
-                        "limit": 100,
+                        "description": description,
+                        "limit": limit,
                         "ticketsavailable": True,
                         "minDate": start_date.strftime("%Y-%m-%d"),
                         "maxDate": stop_date.strftime("%Y-%m-%d")
@@ -30,8 +32,20 @@ def get_events(lat, long, rad, eventcode="LIVE", start_date=datetime.datetime.no
 
 
 
-events = get_events(53.4839, -2.2445, 10, "LIVE", datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=10))
+events = get_events(51.481583, -3.179090, 10, "LIVE", datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=100))
+to_add = []
+spotify = spotipy.Spotify(auth=util.prompt_for_user_token('milesbudden4','playlist-modify-public user-read-private playlist-modify-private playlist-read-private'))
 if events:
-    pprint(events)
+    for event in events:
+        result = spotify.artist(event)
+        print(result["name"])
+        response = spotify.artist_top_tracks(event)
+        # pprint(response['tracks'])
+        for track in response['tracks'][:3]:
+            print("- ", track['name'])
+            to_add.append(track['uri'])
+        spotify.user_playlist_add_tracks('milesbudden4', 'spotify:playlist:65VgQko1QxehfdiI6rcfKn', to_add)
+        to_add = []
+
 else:
     raise Exception("No gigs found")
