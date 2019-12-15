da56c1696305383af2ff58fe83ac3644741a84e6
Miles Budden <miles@budden.net>
2019-12-15 22:19:21 +0000

    Added config file

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import datetime
 from pprint import pprint
 import spotipy
 import spotipy.util as util
+import yaml
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+print(" ".join(cfg['scope']))
 
 def get_events(lat, long, rad, eventcode="LIVE", start_date=datetime.datetime.now(), stop_date=datetime.datetime.now()+datetime.timedelta(days=10), description=1, limit=100):
     r = requests.get("https://www.skiddle.com/api/v1/events/search/",
@@ -32,24 +38,32 @@ def get_events(lat, long, rad, eventcode="LIVE", start_date=datetime.datetime.no
 
 
 
-events = get_events(51.481583,
-                    -3.179090,
-                    10,
+events = get_events(cfg['location']['lat'],
+                    cfg['location']['long'],
+                    cfg['location']['radius'],
                     "LIVE",
                     datetime.datetime.now(),
                     datetime.datetime.now() + datetime.timedelta(days=100))
-to_add = []
-spotify = spotipy.Spotify(auth=util.prompt_for_user_token('milesbudden4',
-                                                          'playlist-modify-public user-read-private playlist-modify-private playlist-read-private'))
 
-playlist = spotify.user_playlist('milesbudden4',
-                                 'spotify:playlist:65VgQko1QxehfdiI6rcfKn')
+events += get_events(cfg['location']['lat'],
+                    cfg['location']['long'],
+                    cfg['location']['radius'],
+                    "FEST",
+                    datetime.datetime.now(),
+                    datetime.datetime.now() + datetime.timedelta(days=100))
+
+to_add = []
+spotify = spotipy.Spotify(auth=util.prompt_for_user_token(cfg['username'],
+                                                          " ".join(cfg['scope'])))
+
+playlist = spotify.user_playlist(cfg['username'],
+                                 cfg['playlist'])
 uris = []
 for track in playlist["tracks"]["items"]:
     uris.append(track["track"]["uri"])
 
-spotify.user_playlist_remove_all_occurrences_of_tracks('milesbudden4',
-                                                       'spotify:playlist:65VgQko1QxehfdiI6rcfKn',
+spotify.user_playlist_remove_all_occurrences_of_tracks(cfg['username'],
+                                                       cfg['playlist'],
                                                        uris)
 if events:
     for event in events:
@@ -60,8 +74,8 @@ if events:
         for track in response['tracks'][:3]:
             print("- ", track['name'])
             to_add.append(track['uri'])
-        spotify.user_playlist_add_tracks('milesbudden4',
-                                         'spotify:playlist:65VgQko1QxehfdiI6rcfKn',
+        spotify.user_playlist_add_tracks(cfg['username'],
+                                         cfg['playlist'],
                                          to_add)
         to_add = []
 
